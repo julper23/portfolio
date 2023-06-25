f33dd55e8560b341f5ff13d49c4a9f23219cb85c
jperezcoca <jperezcoca@ceit.es>
2023-06-25 14:17:01 +0200

    añadido fondo al body

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 export function App () {
     return (
-        <h1>
-            Hello word!
-        </h1>
+        <main id="main">
+        <header></header>
+        <section>Personal: Acerca de mi, CV, redes sociales</section>
+        <section>Habilidades</section>
+        <section>Projectos recientes</section>
+        <section>Contacto</section>
+    </main>
     )
 }
