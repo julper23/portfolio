1931641524b64fc906d7282a9fa21594210bd232
jperezcoca <jperezcoca@ceit.es>
2023-07-03 21:36:21 +0200

    mejorado cambio de texto

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,65 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import './App.css';
 import TextoCambiante from './componentes/textoCambiante/index.jsx'
 import colors from "./../theme/colors";
 import miFoto from "../assets/fotoPersonal.jpeg"
+import ReactTextTransition, { presets } from "react-text-transition";
+
 export function App () {
 
-    const meGusta = [
-      "trabajar en equipo",
-      "aprender",
-      "resolver problemas",
-      "programar"
-    ]
-    
-    const [textMeGusta,setTextMeGusta] = useState(0);
-    
-    const delay = () => new Promise(res => setTimeout(res, 2000));
-    
-    const cambiarMeGusta = async(i) => {
-      await delay()
-      
-      if(i === meGusta.length -1 ) {
-        setTextMeGusta(0)
-      }else{
-        setTextMeGusta(i+1)
-      }
-    }
-    
-    useEffect(()=>{
-      cambiarMeGusta(textMeGusta)
-      // eslint-disable-next-line
-    },[textMeGusta])
-    
+  const meGusta = [
+    "trabajar en equipo",
+    "aprender",
+    "resolver problemas",
+    "programar"
+  ]
 
-    
-    return (
-      <main>
-        <header style={{width:"100%",minWidth:"150px",backgroundColor:"black",height:25}}></header>
-        <section style={{width:"100%",minWidth:"150px",minHeight:200,backgroundColor:colors.meta,display:"flex",flexDirection:"row",justifyContent:"center",flexWrap:"wrap"}}>
-          <div id="FotoPersonal" style={{height:"auto",display:"flex",alignContent:"center",alignItems:"center"}}>
-            <img alt="imagen de julen perez" src={miFoto} style={{width:"150px",height:"150px",borderRadius:100}}/>
-          </div>
-          <div id="InfoPersonal" style={{height:"auto",display:"flex",flexDirection:"column",justifyContent:"center",padding:25}}>
-            <span>¡Bienvenido!</span>
-            <span>Soy Julen Pérez Coca, Junior Front-End developer</span>
-            <div><span>Y me gusta </span><TextoCambiante textoArray={meGusta}/></div>
-            
-          </div>
-        </section>
-        {/*
-        <section>Personal: Acerca de mi, CV, redes sociales</section>
-        <section>Habilidades</section>
-        <section>Projectos recientes</section>
-        <section>Contacto</section>
-        */}
-    </main>
-    )
+  const intvl = useRef(null);
+
+  const [textIndex, setTextIndex] = useState(0);
+
+  const inc = () => {
+    setTextIndex(preVal => preVal===meGusta.length-1?0:preVal+1);
+  };
+
+  useEffect(() => {
+    intvl.current = setInterval(inc, 2000);
+    return () => {
+      clearInterval(intvl.current);
+    };
+  }, []);
+
+
+  return (
+    <main>
+      <section style={{width:"100%",minWidth:"150px",minHeight:200,backgroundColor:colors.meta,display:"flex",flexDirection:"row",justifyContent:"center",flexWrap:"wrap"}}>
+        <div id="FotoPersonal" style={{height:"auto",display:"flex",alignContent:"center",alignItems:"center"}}>
+          <img alt="imagen de julen perez" src={miFoto} style={{width:"150px",height:"150px",borderRadius:100}}/>
+        </div>
+        <div id="InfoPersonal" style={{height:"auto",display:"flex",flexDirection:"column",justifyContent:"center",padding:25}}>
+          <span>¡Bienvenido!</span>
+          <span>Soy Julen Pérez Coca, Junior Front-End developer</span>
+          <section>
+            <section className="inline">
+              Y me gusta
+              <ReactTextTransition
+                springConfig={presets.gentle}
+                style={{ margin: "0 4px",color:"#0000FF",fontSize:"large",fontWeight:"bold" }}
+                inline
+              >
+                {meGusta[textIndex]}
+              </ReactTextTransition>
+            </section>
+          </section>
+        </div>
+      </section>
+      <secton></secton>
+      {/*
+      <section>Personal: Acerca de mi, CV, redes sociales</section>
+      <section>Habilidades</section>
+      <section>Projectos recientes</section>
+      <section>Contacto</section>
+      */}
+  </main>
+  )
 }
---
+++ src/componentes/header/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header = () => {
+  return(<header style={{width:"100%",minWidth:"150px",backgroundColor:"black",height:25}}></header>)
+}
+
+export default Header
