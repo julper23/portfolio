8641aa1f839da5410dfc1fe37a67eae6bc66a6d5
jperezcoca <jperezcoca@ceit.es>
2023-07-05 20:17:17 +0200

    añadidos botones de redes sin enlaces ni logos

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,66 @@ export function App () {
     "programar"
   ]
 
+  const redesSociales = [
+    {
+      name:"CV",
+      logo:"C"
+    },
+    {
+      name:"Linkeding",
+      logo:"L"
+    },
+    {
+      name:"Instagram",
+      logo:"I"
+    }
+  ]
+
+  const habilidades = [
+    {
+      name:"CSS",
+      logo:"CSS"
+    },
+    {
+      name:"HTML",
+      logo:"HTML"
+    },
+    {
+      name:"Java",
+      logo:"Java"
+    },
+    {
+      name:"JavaScript",
+      logo:"JS"
+    },
+    {
+      name:"MySQL",
+      logo:"MS"
+    },
+    {
+      name:"PHP",
+      logo:"PHP"
+    },
+    {
+      name:"Python",
+      logo:"P"
+    },
+    {
+      name:"React",
+      logo:"R"
+    },
+    {
+      name:"React Native",
+      logo:"R N"
+    },
+    {
+      name:"TypeScript",
+      logo:"TS"
+    }
+  ]
+
+
+
   const intvl = useRef(null);
 
   const [textIndex, setTextIndex] = useState(0);
@@ -30,10 +90,21 @@ export function App () {
     };
   }, []);
 
+  const BotonSocial = ({text}) => {
+    return(
+      <button style={{height:"50px",width:"50px",margin:"2px",borderRadius:"15px",cursor:"pointer"}} onClick={()=>{console.log(text);}}>{text}</button>
+    )
+  }
+
+  const Separador = () => {
+    return<div style={{height:"15px",backgroundColor:colors.primary}}/>
+  }
 
   return (
     <main>
       <Header/>
+
+      {/*SECCION PRINCIPAL*/}
       <div style={{width:"100%",minWidth:"150px",minHeight:200,backgroundColor:colors.meta,display:"flex",flexDirection:"row",justifyContent:"center",flexWrap:"wrap"}}>
         <div id="FotoPersonal" style={{height:"auto",display:"flex",alignContent:"center",alignItems:"center"}}>
           <img alt="imagen de julen perez" src={miFoto} style={{width:"150px",height:"150px",borderRadius:100}}/>
@@ -55,25 +126,43 @@ export function App () {
           </div>
         </div>
       </div>
-      <div style={{height:"15px",backgroundColor:colors.primary}}/>
-      <div style={{width:"100%",minWidth:"150px",minHeight:200,backgroundColor:colors.meta,display:"flex",flexDirection:"row",justifyContent:"center",flexWrap:"wrap"}}>
+
+    <Separador/>
+
       {/*REDES SOCIALES Y CV*/}
+      <div style={{width:"100%",minWidth:"150px",height:60,backgroundColor:colors.meta,display:"flex",flexDirection:"row",justifyContent:"center",alignContent:"center",flexWrap:"wrap"}}>
+        {redesSociales.map((rrss)=>{
+          return(<BotonSocial key={rrss.name} text={rrss.logo}/>)
+        })}
       </div>
-      <div style={{height:"15px",backgroundColor:colors.primary}}/>
-      <div style={{width:"100%",minWidth:"150px",minHeight:200,backgroundColor:colors.meta,display:"flex",flexDirection:"row",justifyContent:"center",flexWrap:"wrap"}}>
-      {/*Habilidades */}
+    <Separador/>
+        {/*Habilidades */}
+      <div style={{width:"100%",minWidth:"150px",minHeight:200,backgroundColor:colors.meta,display:"flex",flexDirection:"row",justifyContent:"center",alignContent:"center",flexWrap:"wrap"}}>
+        {habilidades.map((habilidad)=>{
+          return(<BotonSocial key={habilidad.name} text={habilidad.logo}/>)
+        })}
       </div>
-      <div style={{height:"15px",backgroundColor:colors.primary}}/>
-      <div style={{width:"100%",minWidth:"150px",minHeight:200,backgroundColor:colors.meta,display:"flex",flexDirection:"row",justifyContent:"center",flexWrap:"wrap"}}>
+    <Separador/>
       {/*Projectos recientes*/}
-      </div>
-      <div style={{height:"15px",backgroundColor:colors.primary}}/>
       <div style={{width:"100%",minWidth:"150px",minHeight:200,backgroundColor:colors.meta,display:"flex",flexDirection:"row",justifyContent:"center",flexWrap:"wrap"}}>
+
+      </div>
+    <Separador/>
       {/*Contacto*/}
+      <div style={{width:"100%",minWidth:"150px",minHeight:200,backgroundColor:colors.meta,display:"flex",flexDirection:"row",justifyContent:"center",flexWrap:"wrap"}}>
+
       </div>
       {/*
+
+      TERMINADO-------
+
+
+      EN PROCESO------
       <section>Personal: Acerca de mi, CV, redes sociales</section>
       <section>Habilidades</section>
+
+
+      POR EMPEZAR-----
       <section>Projectos recientes</section>
       <section>Contacto</section>
       */}
