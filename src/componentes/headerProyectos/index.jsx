e8fc2e0648649cbbbddc9a13a36e5076a21367b0
jperezcoca <jperezcoca@ceit.es>
2023-07-26 00:10:13 +0200

    corregido bug ruleta y creado hook y componetizado

---
+++ src/componentes/headerProyectos/index.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import './style.css';
+
+import BotonVolver from '../botonVolver';
+
+const HeaderProyectos = () => {
+  return(<header className="header"><BotonVolver/></header>)
+}
+
+export default HeaderProyectos
